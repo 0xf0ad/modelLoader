15fce456778f7c27208c686a06bff5cd54764964
Hamza <wachdarorihadchi@protonmail.ch>
2022-06-07 16:03:42 +0100

    add an overlay to show fps an mouse posation

--- main.cpp
+++ main.cpp
@@ -23,9 +23,13 @@ float lastX = WIN_WIDTH  >> 1; //deviding the width by 2 (but divition is expens
 float lastY = WIN_HEIGHT >> 1; //insted we will shift the width by 1 witch save us some CPU cycles)
 bool firstMouse = true;
 
+bool showOverlay = true;
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void processInput(GLFWwindow *window);
+static void ShowExampleAppSimpleOverlay(bool* p_open);
+
 int main(int argc, char** argv){
 	
 	//check for number of arguments
@@ -167,6 +171,8 @@ int main(int argc, char** argv){
 			ImGui::End();
 		}
 
+		ShowExampleAppSimpleOverlay(&showOverlay);
+
 		//enable shader before setting uniforms
 		ourShader.use();
 
@@ -278,4 +284,43 @@ void mouse_callback(GLFWwindow* window, double xposIn, double yposIn){
 	}if(glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_RIGHT) == GLFW_RELEASE){
 		glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
 	}
+}
+
+static void ShowExampleAppSimpleOverlay(bool* p_open){
+	static int corner = 0;
+	ImGuiIO& io = ImGui::GetIO();
+	ImGuiWindowFlags window_flags = ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_NoFocusOnAppearing | ImGuiWindowFlags_NoNav;
+	const float PAD = 10.0f;
+	const ImGuiViewport* viewport = ImGui::GetMainViewport();
+	ImVec2 work_pos = viewport->WorkPos; // Use work area to avoid menu-bar/task-bar, if any!
+	ImVec2 work_size = viewport->WorkSize;
+	ImVec2 window_pos, window_pos_pivot;
+	window_pos.x = (corner & 1) ? (work_pos.x + work_size.x - PAD) : (work_pos.x + PAD);
+	window_pos.y = (corner & 2) ? (work_pos.y + work_size.y - PAD) : (work_pos.y + PAD);
+	window_pos_pivot.x = (corner & 1) ? 1.0f : 0.0f;
+	window_pos_pivot.y = (corner & 2) ? 1.0f : 0.0f;
+	ImGui::SetNextWindowPos(window_pos, ImGuiCond_Always, window_pos_pivot);
+	window_flags |= ImGuiWindowFlags_NoMove;
+	ImGui::SetNextWindowBgAlpha(0.35f); // Transparent background
+
+	if (ImGui::Begin("Simple Overlay", p_open, window_flags)){
+		ImGui::Text("debugging overlay\n" "in the corner of the screen.\n" "(right-click to change position)");
+		ImGui::Separator();
+
+		if (ImGui::IsMousePosValid()){
+			ImGui::Text("Mouse Position: (%.1f,%.1f)", io.MousePos.x, io.MousePos.y);
+		}else{
+			ImGui::Text("Mouse Position: <invalid>");}
+
+		ImGui::Text("App average %.3f ms/frame\n framerate : (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+
+		if (ImGui::BeginPopupContextWindow()){
+			if (ImGui::MenuItem("Top-left",     NULL, corner == 0)) corner = 0;
+			if (ImGui::MenuItem("Top-right",    NULL, corner == 1)) corner = 1;
+			if (ImGui::MenuItem("Bottom-left",  NULL, corner == 2)) corner = 2;
+			if (ImGui::MenuItem("Bottom-right", NULL, corner == 3)) corner = 3;
+			ImGui::EndPopup();
+		}
+	}
+	ImGui::End();
 }
